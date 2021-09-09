398b06641c7a0a342e95ede73d9779039a0b828e
tairyu <koudoldk@icloud.com>
2021-09-09 15:38:16 +0900

    tt

--- resources/views/emails/appro_body.blade.php
+++ resources/views/emails/appro_body.blade.php
@@ -4,7 +4,7 @@
 <p>ユーザー名：{{ $user_name }}</p>
 <br>
 <p>承認用URL：</p>
-@if(app('env') != production)
+@if(app('env') != 'production')
     <p><a href="http://localhost:8000/content/appro?token={{ $remember }}">http://localhost:8000/content/appro?token={{ $remember }}</a></p>
 @else
     <p><a href="https://submarine21.herokuapp.com/content/appro?token={{ $remember }}">https://submarine21.herokuapp.com/content/appro?token={{ $remember }}</a></p>
