4fb098cff4b29b427c137c26be48c36a1691b602
tairyu <koudoldk@icloud.com>
2021-09-09 19:11:32 +0900

    mail_rep

--- resources/views/emails/rep_body.blade.php
+++ resources/views/emails/rep_body.blade.php
@@ -3,3 +3,4 @@
 <p>コンテンツ投稿のリクエストが承認されました。</p>
 <p>現在、あと{{ $plus_count }}回投稿できます。</p>
 <p>回数が切れた場合、再び承認リクエストを送ってください。</p>
+<p><a href="https://submarine21.herokuapp.com/home">https://submarine21.herokuapp.com/home</a></p>
