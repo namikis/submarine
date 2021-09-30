ff90df501b72349eaf9ac0ea9f9910ed2c73b1b8
tairyu <koudoldk@icloud.com>
2021-09-30 15:10:30 +0900

    pg

--- app/auto/get_auto_heroku.php
+++ app/auto/get_auto_heroku.php
@@ -64,23 +64,19 @@ namespace App\auto;
             $DB_name = $_ENV['DB_DATABASE'];
             $DB_conn = $_ENV['DB_CONNECTION'];
           
-            // $dsn = $DB_conn.':dbname=' . $DB_name . ';host=' . $host . ";port=5432";
-            // $dbh = new PDO($dsn, $user, $pass);
-            // $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            // $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-            // $sql = "SET CLIENT_ENCODING TO 'UTF-8';";
-            // $stmt = $dbh->prepare($sql);
-            // $stmt->execute();
+            $dsn = $DB_conn.':dbname=' . $DB_name . ';host=' . $host . ";port=5432";
+            $dbh = new PDO($dsn, $user, $pass);
+            $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            $sql = "SET CLIENT_ENCODING TO 'UTF-8';";
+            $stmt = $dbh->prepare($sql);
+            $stmt->execute();
 
-            // $image_url = $data['image_url'];
+            $image_url = $data['image_url'];
             $sql = "select count(id) as count from auto_contents where image_url = '" . $image_url . "'";
-            // $stmt = $dbh->prepare($sql);
-            // $stmt->execute();
-            // $rec = $stmt->fetchAll();
-            $conn = "host=".$host." dbname=".$DB_name." user=".$user." password=".$pass;
-            $link = pg_connect($conn);
-            pg_set_client_encoding($link, "UNICODE");
-            $result = pg_query($link, $sql);
+            $stmt = $dbh->prepare($sql);
+            $stmt->execute();
+            $rec = $stmt->fetchAll();
 
             $rec = pg_fetch_array($result, NULL, PGSQL_ASSOC);
             if($rec['count'] == 0){
@@ -97,25 +93,16 @@ namespace App\auto;
             $DB_name = $_ENV['DB_DATABASE'];
             $DB_conn = $_ENV['DB_CONNECTION'];
           
-            // $dsn = $DB_conn.':dbname=' . $DB_name . ';host=' . $host . ";port=5432";
-            // $dbh = new PDO($dsn, $user, $pass);
-            // $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            // $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-            $conn = "host=".$host." dbname=".$DB_name." user=".$user." password=".$pass;
-            $link = pg_connect($conn);
-            pg_set_client_encoding($link, "UNICODE");
+            $dsn = $DB_conn.':dbname=' . $DB_name . ';host=' . $host . ";port=5432";
+            $dbh = new PDO($dsn, $user, $pass);
+            $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
             if(checkExist($data) == 1 && $data['image_url'] != ''){
               $sql = genInsertQuery($data, "auto_contents");
-              // $stmt = $dbh->prepare($sql);
-              // $stmt->execute();
-              echo "\n" . $data['content_detail'] . "\n";
-              echo "\n" . $sql . "\n";
-              $res = pg_query($link, $sql);
-
-              $sql = "select max(id) as lastId from auto_contents";
-              $res = pg_query($link, $sql);
-              $rec = pg_fetch_array($res, NULL, PGSQL_ASSOC);
+              $stmt = $dbh->prepare($sql);
+              $stmt->execute();
+              $content_id = $dbh->lastInsertId();
   
               $content_id = $rec['lastId'];
               $data2 = array(
@@ -123,10 +110,9 @@ namespace App\auto;
                   "tag" => $tag
               );
               $sql = genInsertQuery($data2, "auto_tag");
-              pg_query($link, $sql);
   
-              // $stmt = $dbh->prepare($encoding . $sql);
-              // $stmt->execute();
+              $stmt = $dbh->prepare($encoding . $sql);
+              $stmt->execute();
             }
           }
 
